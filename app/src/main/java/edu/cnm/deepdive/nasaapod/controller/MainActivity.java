d78b525db7dbe73b0949037e8d27d773d583d98b
Nick Bennett <nick@nickbenn.com>
2018-11-01 10:04:30 -0600

    Defined service & model.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,15 +1,24 @@
-package edu.cnm.deepdive.nasaapod;
+package edu.cnm.deepdive.nasaapod.controller;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.nasaapod.BuildConfig;
+import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.model.Apod;
+import edu.cnm.deepdive.nasaapod.service.ApodService;
 import java.util.Calendar;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +31,16 @@ public class MainActivity extends AppCompatActivity {
   private ProgressBar progressSpinner;
   private FloatingActionButton jumpDate;
   private Calendar calender;
+  private ApodService service;
+  private Apod apod;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setupWebView();
-    // TODO Setup service
-    // TODO Setup UI
+    setupService();
+    setupUI();
     // TODO Setup defaults
   }
 
@@ -55,7 +66,35 @@ public class MainActivity extends AppCompatActivity {
     settings.setLoadWithOverviewMode(true);
   }
 
+  private void setupUI() {
+    progressSpinner = findViewById(R.id.progress_spinner);
+    progressSpinner.setVisibility(View.GONE);
+    jumpDate = findViewById(R.id.jump_date);
+    jumpDate.setOnClickListener(new OnClickListener() {
+      // TODO Use lambda form.
+      @Override
+      public void onClick(View v) {
+        // TODO Display date picker.
+      }
+    });
+  }
+
+  private void setupService() {
+    Gson gson = new GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .setDateFormat(DATE_FORMAT)
+        .create();
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(getString(R.string.base_url))
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build();
+    service = retrofit.create(ApodService.class);
+    apiKey = BuildConfig.API_KEY;
+  }
+
 }
+
+
 
 
 
